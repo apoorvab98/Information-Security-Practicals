146165b2cae8741db9580e5c49a86d953d716a10
Apoorva Bidyanta <47899021+apoorvab98@users.noreply.github.com>
2020-01-06 23:19:54 +0530

    Add files via upload

---
+++ Autokey_ispractical_2.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+import java.util.Random;
+import java.util.Scanner;
+public class Autokey_ispractical_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String k,pl,cipher;
+        System.out.println("Enter the plain text w/o spaces:");
+        pl=sc.next();
+        String p="";
+        for(int i=0;i<pl.length();i++)
+        {  char ch=pl.charAt(i);
+           ch=Character.toUpperCase(ch);
+           p+=ch;        }
+        Random rand=new Random();
+        k="";
+        for(int i=0;i<p.length();i++){
+            int r=rand.nextInt(26);
+            r=r+65;
+            k+=(char)r;
+        }
+        cipher="";
+        for(int i=0;i<k.length();i++){
+            int sum=(int)(p.charAt(i))-65+(int)(k.charAt(i))-65;
+            sum=sum%26;
+            sum+=65;
+            cipher+=(char)sum;
+        }
+        System.out.println("Random Key:-"+k);
+        System.out.println("Cipher Text:-"+cipher);
+        String decipher="";
+        for(int i=0;i<k.length();i++){
+            int sum=(int)cipher.charAt(i)-(int)k.charAt(i);
+            if(sum<0)
+                sum=sum+26;
+            sum=sum%26;
+            sum=sum+65;
+            decipher+=(char)sum;
+        }
+        System.out.println("Deciphered text:-"+decipher);
+    }
+}
